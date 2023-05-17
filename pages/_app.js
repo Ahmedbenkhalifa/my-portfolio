3987347d001d0f836478ef44313cc99ad2d1cfd3
Ahmedbenkhalifa <ahmedbenkhalifa1997@gmail.com>
2023-05-17 22:35:55 +0100

    start it

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "../styles/theme";
+import "../styles/globals.css";
+import Navbar from "../components/Navbar/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
